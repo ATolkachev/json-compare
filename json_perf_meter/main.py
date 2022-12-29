fbb50e5ccd88dc09b4c368cd25bbe1e23b1b4742
Aleksandr Tolkachev <aleksandr.tolkachev@sap.com>
2022-12-29 16:39:44 +0100

    First iteration done

--- json_perf_meter/main.py
+++ json_perf_meter/main.py
@@ -2,8 +2,12 @@ import os
 import sys
 import time
 
-with open("{}/imdb.json".format(os.getcwd()), "r") as f:
+with open("{}/data.json".format(os.getcwd()), "r") as f:
     data = f.read()
+
+
+def report(module, action, exec_time):
+    print("{} {} time: {:.2f}".format(module, action, exec_time))
 
 
 def calc_load_json(data):
@@ -11,7 +15,7 @@ def calc_load_json(data):
     start_time = time.monotonic()
     load = json.loads(data)
     exec_time = time.monotonic() - start_time
-    print("json load time: {}".format(exec_time))
+    report(json.__name__, 'load', exec_time)
     return load
 
 
@@ -20,7 +24,7 @@ def calc_dump_json(data):
     start_time = time.monotonic()
     load = json.dumps(data)
     exec_time = time.monotonic() - start_time
-    print("json dump time: {}".format(exec_time))
+    report(json.__name__, 'dump', exec_time)
     return load
 
 
@@ -29,7 +33,16 @@ def calc_load_ujson(data):
     start_time = time.monotonic()
     load = json.loads(data)
     exec_time = time.monotonic() - start_time
-    print("ujson load time: {}".format(exec_time))
+    report(json.__name__, 'load', exec_time)
+    return load
+
+
+def calc_dump_ujson(data):
+    import ujson as json
+    start_time = time.monotonic()
+    load = json.dumps(data)
+    exec_time = time.monotonic() - start_time
+    report(json.__name__, 'dump', exec_time)
     return load
 
 
@@ -38,7 +51,16 @@ def calc_load_nujson(data):
     start_time = time.monotonic()
     load = json.loads(data)
     exec_time = time.monotonic() - start_time
-    print("nujson load time: {}".format(exec_time))
+    report(json.__name__, 'load', exec_time)
+    return load
+
+
+def calc_dump_nujson(data):
+    import nujson as json
+    start_time = time.monotonic()
+    load = json.dumps(data)
+    exec_time = time.monotonic() - start_time
+    report(json.__name__, 'dump', exec_time)
     return load
 
 
@@ -47,7 +69,16 @@ def calc_load_orjson(data):
     start_time = time.monotonic()
     load = json.loads(data)
     exec_time = time.monotonic() - start_time
-    print("orjson load time: {}".format(exec_time))
+    report(json.__name__, 'load', exec_time)
+    return load
+
+
+def calc_dump_orjson(data):
+    import orjson as json
+    start_time = time.monotonic()
+    load = json.dumps(data)
+    exec_time = time.monotonic() - start_time
+    report(json.__name__, 'dump', exec_time)
     return load
 
 
@@ -56,7 +87,16 @@ def calc_load_simplejson(data):
     start_time = time.monotonic()
     load = json.loads(data)
     exec_time = time.monotonic() - start_time
-    print("simplejson load time: {}".format(exec_time))
+    report(json.__name__, 'load', exec_time)
+    return load
+
+
+def calc_dump_simplejson(data):
+    import simplejson as json
+    start_time = time.monotonic()
+    load = json.dumps(data)
+    exec_time = time.monotonic() - start_time
+    report(json.__name__, 'dump', exec_time)
     return load
 
 
@@ -65,7 +105,16 @@ def calc_load_hjson(data):
     start_time = time.monotonic()
     load = json.loads(data)
     exec_time = time.monotonic() - start_time
-    print("hjson load time: {}".format(exec_time))
+    report(json.__name__, 'load', exec_time)
+    return load
+
+
+def calc_dump_hjson(data):
+    import hjson as json
+    start_time = time.monotonic()
+    load = json.dumps(data)
+    exec_time = time.monotonic() - start_time
+    report(json.__name__, 'dump', exec_time)
     return load
 
 
@@ -74,21 +123,30 @@ def calc_load_rapidjson(data):
     start_time = time.monotonic()
     load = json.loads(data)
     exec_time = time.monotonic() - start_time
-    print("rapidjson load time: {}".format(exec_time))
+    report(json.__name__, 'load', exec_time)
+    return load
+
+
+def calc_dump_rapidjson(data):
+    import rapidjson as json
+    start_time = time.monotonic()
+    load = json.dumps(data)
+    exec_time = time.monotonic() - start_time
+    report(json.__name__, 'dump', exec_time)
     return load
 
 
 if sys.argv[1] == 'json':
     calc_dump_json(calc_load_json(data))
 elif sys.argv[1] == 'ujson':
-    calc_load_ujson(data)
+    calc_dump_ujson(calc_load_ujson(data))
 elif sys.argv[1] == 'nujson':
-    calc_load_nujson(data)
+    calc_dump_nujson(calc_load_nujson(data))
 elif sys.argv[1] == 'hjson':
-    calc_load_hjson(data)
+    calc_dump_hjson(calc_load_hjson(data))
 elif sys.argv[1] == 'orjson':
-    calc_load_orjson(data)
+    calc_dump_orjson(calc_load_orjson(data))
 elif sys.argv[1] == 'python-rapidjson':
-    calc_load_rapidjson(data)
+    calc_dump_rapidjson(calc_load_rapidjson(data))
 elif sys.argv[1] == 'simplejson':
-    calc_load_simplejson(data)
+    calc_dump_simplejson(calc_load_simplejson(data))
--- test.sh
+++ test.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+
+[ ! -f "data.json" ] && curl "https://data.lacity.org/api/geospatial/yru6-6re4?method=export&format=GeoJSON" -o data.json
+python json_perf_meter/main.py json
 for i in $(cat requirements.txt); do python json_perf_meter/main.py $i; done
